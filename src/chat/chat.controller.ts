702b92205a64b5224e0bf869cfbd08835d10cdbf
bitue <ashikul.islam_ashik@yahoo.com>
2025-06-01 04:01:55 -0700

    chat hisotry token add

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -7,15 +7,18 @@ import {
   Param,
   Delete,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { Chat } from './entities/chat.entity';
+import { AuthGuard } from '@/common/guards/auth.guard';
 
 @Controller('chat')
 export class ChatController {
   constructor(private readonly chatService: ChatService) {}
 
   @Get('history')
+  @UseGuards(AuthGuard)
   async getChatHistory(
     @Query('sender') sender: number,
     @Query('receiver') receiver: number,
