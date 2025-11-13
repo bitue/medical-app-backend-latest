7d6fa33fe0f34f0d09f97e75b24aa8b4624bfb21
bitue <ashikul.islam_ashik@yahoo.com>
2025-11-13 11:26:22 +0600

    pref : education controller cleanup

--- src/education/education.controller.ts
+++ src/education/education.controller.ts
@@ -18,48 +18,4 @@ import { Roles } from '@/common/decorators/roles.decorator';
 @Controller('education')
 export class EducationController {
   constructor(private readonly educationService: EducationService) {}
-
-  // @Post()
-  // @UseGuards(AuthGuard, RoleGuard)
-  // @Roles('doctor')
-  // async create(
-  //   @Body() educationData: CreateEducationDto,
-  //   @CurrentUser() user: User,
-  // ) {
-  //   try {
-  //     console.log(user, 'from  current user ');
-  //     const [existingDoctor, education] = await Promise.all([
-  //       this.doctorService.findByUserId(user?.id),
-  //       this.educationService.create(educationData),
-  //     ]);
-
-  //     // if (!existingDoctor) {
-  //     //   await this.doctorService.create({ user: user, education });
-  //     //   return {
-  //     //     code: '201',
-  //     //     message: 'Education data created successfully!',
-  //     //     data: education,
-  //     //     status: true,
-  //     //   };
-  //     // }
-
-  //     if (!existingDoctor) {
-  //       throw new BadRequestException(
-  //         'Doctor profile not found. Please create doctor profile first.',
-  //       );
-  //     }
-
-  //     await this.doctorService.update(existingDoctor.id, {
-  //       educations: [...existingDoctor.educations, education],
-  //     });
-  //     return {
-  //       code: '201',
-  //       message: `Education data added  successfully to ${user.username} profile `,
-  //       data: education,
-  //       status: true,
-  //     };
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // }
 }
